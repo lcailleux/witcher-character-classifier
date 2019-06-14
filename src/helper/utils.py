d2ef6508426cad1e0ed40f4bdf7240532dbd9dc9
lue <louiscailleux@gmail.com>
2019-06-14 16:21:13 +0200

    Adding requirements.txt. fixing warnings.

--- src/helper/utils.py
+++ src/helper/utils.py
@@ -1,19 +1,20 @@
 import numpy as np
-from PyQt5 import QtGui
+from PyQt5.QtGui import QImage
 
 
 def ndarray_to_qimage(image):
     """
 
     :param image: image data (numpy array)
-    :return:
+    :return image: QImage
     """
+
     assert (np.max(image) <= 255)
     image8 = image.astype(np.uint8, order='C', casting='unsafe')
     height, width, colors = image8.shape
     bytes_per_line = 3 * width
 
-    image = QtGui.QImage(image8.data, width, height, bytes_per_line, QtGui.QImage.Format_RGB888)
+    image = QImage(image8.data, width, height, bytes_per_line, QImage.Format_RGB888)
     image = image.rgbSwapped()
 
     return image
